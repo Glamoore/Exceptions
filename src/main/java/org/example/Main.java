818a2b1c65142acba7a31b603e6ed86a40f0e1f9
Reece Moore <reecemoore@192.168.1.113>
2024-07-31 12:31:35 +0100

    Completed task 7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,14 @@ package org.example;
 
 import org.example.exercises.Exercise3;
 import org.example.exercises.Exercise4;
+import org.example.exercises.Exercise5;
 import org.example.exercises.NegativeIntegerInputException;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Exercise4.recursiveMethod();
+        Exercise5.healthInsuranceDataInput();
     }
 
 
---
+++ src/main/java/org/example/exercises/Exercise5.java
@@ -0,0 +1,88 @@
+package org.example.exercises;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise5 {
+
+    public static void healthInsuranceDataInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\n--- Thank you for choosing Blummin Health insurance! ---\n");
+        System.out.println("Please input your data:");
+
+        // Name
+        System.out.print("Enter your full name: ");
+        String name = scanner.nextLine();
+        if(name.split(" ").length <= 1) {
+            System.out.println("Please enter First and Surname");
+        }
+        // Age
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        if(age < 0) {
+            System.out.println("Age cannot be negative");
+        }
+
+        // Height
+        System.out.print("Enter your height (in metres): ");
+        double height = scanner.nextDouble();
+
+        // Height
+        System.out.print("Enter your weight (in kilograms): ");
+        double weight = scanner.nextDouble();
+
+        // Employment
+        System.out.print("Are you employed?: ");
+        boolean employed = scanner.nextBoolean();
+        scanner.nextLine();
+        if(employed && age < 16) {
+            System.out.println("You cannot work below 16");
+        }
+
+        // Occupation
+        System.out.print("What is your occupation?: ");
+        String occupation = scanner.nextLine();
+
+        // Student
+        System.out.print("Are you a student?: ");
+        boolean isStudent = scanner.nextBoolean();
+        if(isStudent && employed) {
+            System.out.println("You cannot be employed and a student");
+        }
+
+        System.out.println("\nThank you, " + name + ", for providing your information!");
+        System.out.println("Your Blummin monthly subscription is: £"
+                + calculateSubscriptionCharge(age, height, weight, occupation, isStudent));
+
+        scanner.close();
+    }
+
+    private static double calculateSubscriptionCharge(int age, double height, double weight, String occupation, boolean isStudent) {
+
+        List<Double> factors = List.of(
+                0.5D, // Base rate
+                calculateAgeFactor(age),
+                calculateHeightFactor(height),
+                calculateWeightFactor(weight),
+                occupation.equalsIgnoreCase("doctor") ? 0.9D : 1.0D,
+                isStudent ? 0.8D : 1.0D
+        );
+
+        return factors.stream().reduce(1.0, (a, b) -> a * b);
+    }
+
+    private static double calculateAgeFactor(int age) {
+        return Math.max(100 + (age - 18) * 5, 1);
+    }
+
+    private static double calculateHeightFactor(double height) {
+        return Math.abs(Math.pow(height, 2) - 2);
+    }
+
+    private static double calculateWeightFactor(double weight) {
+        return 2 * weight;
+    }
+
+}

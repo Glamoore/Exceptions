eb12279cea2a8bd494ab192fd06e6cfa35ea89ff
Reece Moore <reecemoore@192.168.1.113>
2024-07-31 11:07:34 +0100

    Feat: completed task 1

---
+++ src/main/java/org/example/exercises/Exercise1.java
@@ -0,0 +1,10 @@
+package org.example.exercises;
+
+public class Exercise1 {
+
+    public static float divide(int number, int dividedBy) {
+        float result;
+        result = number / dividedBy;
+        return result;
+    }
+}
